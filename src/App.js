5a9574b39017fd66be87fcc5f834b40d7f2687c1
tanvirch0750 <tanvir0750@yahoo.com>
2021-04-15 17:34:19 +0600

    added starter file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Dashboard, Error, Login } from './pages';
+
+function App() {
+    return (
+        <div>
+            <Dashboard />
+            <Login />
+            <Error />
+        </div>
+    );
+}
+
+export default App;
