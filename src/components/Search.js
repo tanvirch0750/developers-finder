4b1b16743a2317d6a8b7f3d3aed0619bae390fd9
tanvirch0750 <tanvir0750@yahoo.com>
2021-06-09 23:08:37 +0600

    added how many requests remaining functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { MdSearch } from 'react-icons/md';
 import styled from 'styled-components';
+import { GithubContext } from '../context/context';
 
 const Search = () => {
     const [user, setUser] = React.useState('');
+    const { requests } = React.useContext(GithubContext);
 
     // get things from global context
 
@@ -28,10 +30,10 @@ const Search = () => {
                             value={user}
                             onChange={(e) => setUser(e.target.value)}
                         />
-                        <button type="submit">Search</button>
+                        {requests > 0 && <button type="submit">Search</button>}
                     </div>
                 </form>
-                <h3>Request Remaining 60 / 60</h3>
+                <h3>Request Remaining: {requests} / 60</h3>
             </Wrapper>
         </section>
     );
--- src/context/context.js
+++ src/context/context.js
@@ -1,18 +1,44 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import mockFollowers from './mockData.js/mockFollowers';
 import mockRepos from './mockData.js/mockRepos';
 import mockUser from './mockData.js/mockUser';
 
 const GithubContext = React.createContext();
 
+const rootUrl = 'https://api.github.com';
+
 const GithubProvider = ({ children }) => {
     const [githubUser, setGithubUser] = useState(mockUser);
     const [repos, setRepos] = useState(mockRepos);
     const [followers, setFollowers] = useState(mockFollowers);
 
+    // requests and loading
+    const [requests, setRequests] = useState(0);
+    const [loading, isSetLoading] = useState(false);
+
+    // check request rate
+    const checkRequests = () => {
+        axios(`${rootUrl}/rate_limit`)
+            .then(({ data }) => {
+                const {
+                    rate: { remaining },
+                } = data;
+
+                setRequests(remaining);
+
+                if (remaining === 0) {
+                    // throw an error
+                }
+            })
+            .catch((err) => console.log(err));
+    };
+
+    useEffect(checkRequests, []);
+
     return (
-        <GithubContext.Provider value={{ githubUser, repos, followers }}>
+        <GithubContext.Provider value={{ githubUser, repos, followers, requests }}>
             {children}
         </GithubContext.Provider>
     );
@@ -25,5 +51,3 @@ export { GithubProvider, GithubContext };
 // import mockUser from './mockData.js/mockUser';
 // import mockRepos from './mockData.js/mockRepos';
 // import mockFollowers from './mockData.js/mockFollowers';
-
-// const rootUrl = 'https://api.github.com';
